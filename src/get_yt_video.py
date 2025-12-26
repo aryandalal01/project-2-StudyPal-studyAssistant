af5a02df70f3d91b49366a749efaf7214ccdf8a7
aryandalal01 <aryandalal2001@gmail.com>
2025-12-26 22:12:46 +0530

    streamlit code

---
+++ src/get_yt_video.py
@@ -0,0 +1,8 @@
+from youtubesearchpython import VideosSearch
+
+def get_yt_video_link(query):
+    videos_search = VideosSearch(query=query,limit=3)
+    result = videos_search.result()
+    video_titles = [video['title'] for video in result['result']]
+    video_links = [video['link'] for video in result['result']]
+    return video_titles,video_links
